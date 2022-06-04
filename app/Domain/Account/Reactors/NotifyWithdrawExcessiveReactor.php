f202c1cfe2f32fcb7335720731c80bc8e4f83400
Julio <hi.pazthor@gmail.com>
2022-06-03 19:30:01 -0500

    update test for NotifyWithdrawExcessive

--- app/Domain/Account/Reactors/NotifyWithdrawExcessiveReactor.php
+++ app/Domain/Account/Reactors/NotifyWithdrawExcessiveReactor.php
@@ -2,9 +2,11 @@
 
 namespace App\Domain\Account\Reactors;
 
+use App\Domain\Account\EventQueries\TransactionFilterEventQuery;
 use App\Models\Account;
 use App\Domain\Account\Events\WithdrawExcessiveAmount;
 use App\Mail\WithdrawExcessiveMail;
+use Carbon\Carbon;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Mail;
 use Spatie\EventSourcing\EventHandlers\Reactors\Reactor;
@@ -13,7 +15,13 @@ class NotifyWithdrawExcessiveReactor extends Reactor implements ShouldQueue
 {
     public function __invoke(WithdrawExcessiveAmount $event)
     {
-        $admin = "admin@email.com";
+        $admin = 'admin@email.com';
+        $aggregateRootUuid = $event->aggregateRootUuid();
+
+        $last24Hours = Carbon::now()->subHours(24)->toDateTimeString();
+        $eq = new TransactionFilterEventQuery( $last24Hours,$aggregateRootUuid);
+
+        $eventlast48hours = $eq->getLastEvents();
 
         Mail::to($admin)->send(new WithdrawExcessiveMail());
     }
--- tests/Domain/Account/Reactors/NotifyWithdrawExcessiveReactorTest.php
+++ tests/Domain/Account/Reactors/NotifyWithdrawExcessiveReactorTest.php
@@ -3,28 +3,39 @@
 namespace Tests\Domain\Account\Reactors;
 
 use App\Domain\Account\AccountAggregateRoot;
+use App\Domain\Account\Events\AccountCreated;
+use App\Domain\Account\Events\AccountLimitHit;
+use App\Domain\Account\Events\MoneyAdded;
+use App\Domain\Account\Events\MoneySubtracted;
+use App\Domain\Account\Events\WithdrawExcessiveAmount;
 use App\Domain\Account\Exceptions\CouldNotSubtractMoney;
+use App\Mail\LoanProposalMail;
 use App\Mail\WithdrawExcessiveMail;
 use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 
 class NotifyWithdrawExcessiveReactorTest extends TestCase
 {
+    private const ACCOUNT_UUID = 'accounts-uuid';
+
+    private const ACCOUNT_NAME = 'fake-account';
     /** @test */
-    public function test_send_withdraw_excessive(): void
+    public function it_should_sent_email_if_subtract_15k(): void
     {
         Mail::fake();
 
-        $aggregate = AccountAggregateRoot::retrieve($this->account->uuid)
-            ->addMoney(20000)
-            ->persist();
+        $aggregate = AccountAggregateRoot::retrieve($this->account->uuid);
 
-        $this->assertExceptionThrown(function () use ($aggregate){
-            $aggregate->subtractMoney(10001);
-        }, CouldNotSubtractMoney::class);
+        $aggregate
+            ->addMoney(30000)
+            ->persist();
+        $aggregate->subtractMoney(15000);
+        $aggregate->persist();
+
 
         Mail::assertSent(function (WithdrawExcessiveMail $mail) {
             return $mail->hasTo('admin@email.com');
         });
+
     }
 }
